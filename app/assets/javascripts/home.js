64ccfa70a38e97ee141234a0eb597d54d9f2b9fd
David Karlsson <davka442@gmail.com>
2014-12-29 09:16:46 +0100

    can disable parallax by hiding groups

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   console.log('javascript');
+
+  $(".menu a").click(function() {
+    var id = '#' + $(this).attr('class');
+    console.log(id);
+
+    $('html, body').animate({
+        scrollTop: $('#kuk').offset().top
+    }, 2000);
+
+    $('.init-hide').toggleClass('hidden');
+  });
+
+
+  // @TODO Create animations for front and menu
+  //
 });
