ab01095bf548fec9b376b5c3d26428b8a8e243d1
David Karlsson <davka442@gmail.com>
2015-01-17 18:42:34 +0100

    home controller fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,13 @@ class HomeController < ApplicationController
   def create
     @attendance = Attendance.new( attendance_params )
     
-    HomeMailer.send_registration_email(@attendance).deliver
-    #if @attendance.save 
-      #HomeMailer.send_registration_email(@attendance).deliver
-      #respond_to do |format|
-        #format.js
-      #end
-    #else
-    #end
+    if @attendance.save 
+      HomeMailer.send_registration_email(@attendance).deliver
+      respond_to do |format|
+        format.js
+      end
+    else
+    end
   end
 
 private
